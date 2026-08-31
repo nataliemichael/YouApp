e3d23603bf7a4510e2adee063d73434423728b48
nataliemichael <natalie.michael@student.uts.edu.au>
2026-08-31 15:22:25 +1000

    Initial Commit

---
+++ You/YouApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouApp.swift
+//  You
+//
+//  Created by Natalie Michael on 31/8/2026.
+//
+
+import SwiftUI
+
+@main
+struct YouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YouTests/YouTests.swift
@@ -0,0 +1,19 @@
+//
+//  YouTests.swift
+//  YouTests
+//
+//  Created by Natalie Michael on 31/8/2026.
+//
+
+import Testing
+@testable import You
+
+struct YouTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
